ca35137c3dc7db12d6653634e265f7602e5cd7cc
James Wood <s192019@uos.ac.uk>
2019-01-10 15:59:55 +0000

    LocationProvider Added

--- MWDAssignment2/app/src/main/java/com/jamesdavidwood/mwdassignment2/MainActivity.kt
+++ MWDAssignment2/app/src/main/java/com/jamesdavidwood/mwdassignment2/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.jamesdavidwood.mwdassignment2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivityForResult()
+        startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
@@ -29,7 +31,20 @@ class MainActivity : AppCompatActivity() {
         AuthUI.IdpConfig.EmailBuilder().build(),
         AuthUI.IdpConfig.PhoneBuilder().build(),
         AuthUI.IdpConfig.GoogleBuilder().build()
+    )
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if ( requestCode == RC_SIGN_IN) {
+            if ( resultCode == RESULT_OK) {
+                var user = mAuth.currentUser
+                if (user != null) {
+                    Log.w("USER", user.email)
+                    intent= Intent(this,MapsActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
 
 // Create and launch sign-in intent
 
--- MWDAssignment2/app/src/main/java/com/jamesdavidwood/mwdassignment2/MapsActivity.kt
+++ MWDAssignment2/app/src/main/java/com/jamesdavidwood/mwdassignment2/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.jamesdavidwood.mwdassignment2
 
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +12,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.storage.FirebaseStorage
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    val db = FirebaseFirestore.getInstance()
+    val dbref = FirebaseStorage.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     /**
@@ -36,8 +45,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        try {
+            fusedLocationClient.lastLocation.addOnSuccessListener{location: Location ->
+                val sydney = LatLng(location.latitude, location.longitude)
+                val geoPoint = GeoPoint(location.latitude, location.longitude)
+                mMap.addMarker(MarkerOptions().position(sydney))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+            }
+
+        }
+        catch (ex:SecurityException) {
+
+        }
+
     }
 }
