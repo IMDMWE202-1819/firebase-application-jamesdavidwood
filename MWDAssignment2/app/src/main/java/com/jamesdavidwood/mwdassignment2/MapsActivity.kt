b853653d38f882b37c1f207c761e9d6f912ddbb9
James Wood <s192019@uos.ac.uk>
2019-02-26 12:01:17 +0000

    Maps API Firebase Implementation
    Implementation of the Firebase Storage and Firebase Database.

--- MWDAssignment2/app/src/main/java/com/jamesdavidwood/mwdassignment2/MapsActivity.kt
+++ MWDAssignment2/app/src/main/java/com/jamesdavidwood/mwdassignment2/MapsActivity.kt
@@ -1,10 +1,10 @@
 package com.jamesdavidwood.mwdassignment2
 
-import android.location.Location
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -13,17 +13,19 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.storage.FirebaseStorage
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
     val db = FirebaseFirestore.getInstance()
-    val dbref = FirebaseStorage.getInstance().reference
+    val storageRef = FirebaseStorage.getInstance().reference
 
-    private var imageToUpload:Uri? = null
+    private var imageToUpload: Uri? = null
     private var imageDownload:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,26 +35,41 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
-    
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        Toast.makeText(this, "Hi there! For the best experience enable location services. ", Toast.LENGTH_LONG).show()
+        // Add a marker in Ipswich and move the camera to location of device.
+        val ipswich = LatLng(52.0525, 1.1635)
+        mMap.addMarker(MarkerOptions().position(ipswich).title("Marker at University of Suffolk"))
 
-        // Add a marker in current location and move the camera
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         try {
-            fusedLocationClient.lastLocation.addOnSuccessListener{location: Location ->
-                val currentlocation = LatLng(location.latitude, location.longitude)
-                val geoPoint = GeoPoint(location.latitude, location.longitude)
-                mMap.addMarker(MarkerOptions().position(currentlocation))
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, 12.0f))
+            fusedLocationClient.lastLocation.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val location = it.result
+
+                    if (location != null) {
+                        mMap.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                LatLng(location.latitude, location.longitude),
+                                17.0f
+                            )
+                        )
+                    }
+                }
             }
-
+        } catch (ex: SecurityException) {
+            Log.w("GEO", "security error", ex)
         }
-        catch (ex:SecurityException) {
-            Log.w("Location Error", "Security Error", ex)
-
-        }
-
     }
 }
